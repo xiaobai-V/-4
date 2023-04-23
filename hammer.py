d2d61087c9d5b6e605871eef1d5a044ccde241d9
xiaobai-V <805892347@qq.com>
2023-04-23 15:58:07 +0800

    plot hammer using adc data

--- hammer.py
+++ hammer.py
@@ -1,9 +1,12 @@
 import pygame
 import random
+import PCF8591 as ADC
 
 
 class Hammer():
     def __init__(self, game):
+
+        ADC.setup(0x48)
         self.screen = game.screen
         self.screen_rect = game.screen.get_rect()
 
@@ -25,8 +28,8 @@ class Hammer():
 
     def update(self):
         # 树莓派上改用adc读取
-        self.VRX = 200
-        self.VRY = 200
+        self.VRX = ADC.read(0)
+        self.VRY = ADC.read(1)
         self.x = int(self.screen_rect.width * self.VRX / 255)
         self.y = int(self.screen_rect.height * self.VRY / 255)
         self.position = (self.x, self.y)
