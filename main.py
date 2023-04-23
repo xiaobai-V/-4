21e883ef0c75f0d78d3ef575fdc2386639e91596
xiaobai-V <805892347@qq.com>
2023-04-23 21:34:01 +0800

    auto format

--- main.py
+++ main.py
@@ -8,27 +8,25 @@ from hammer import Hammer
 class Whack_a_mole():
 
     def __init__(self):
-        pygame.init()
+        # 游戏初始化
+        pygame.init()  # pygame初始化
+        self.screen = pygame.display.set_mode((800, 600))  # 设置窗口大小
+        pygame.display.set_caption("Whack a mole")  # 设置窗口标题
 
-        self.screen = pygame.display.set_mode((800, 600))
-        pygame.display.set_caption("Whack a mole")
+        self.background = pygame.image.load("./images/bg.png")  # 加载背景图片
 
-        self.background = pygame.image.load("bg.png")
-
-        self.hammer = Hammer(self)
-
-        self.VRX = 123
-        self.VRY = 123
+        self.hammer = Hammer(self)  # 创建锤子对象
 
     def run_game(self):
         while True:
-            self._check_joystick()
-            self._check_events()
-            self._update_screen()
-            time.sleep(0.5)
+            self._check_joystick()  # 检查摇杆
+            self._check_events()  # 检查事件
+            self._update_screen()  # 更新屏幕
+            time.sleep(0.05)
 
     def _check_joystick(self):
-        pass
+        self.hammer.update()  # 更新锤子位置
+        self.hammer.check_mole()  # 检查锤子是否击中地鼠
 
     def _check_events(self):
         for event in pygame.event.get():
@@ -36,12 +34,12 @@ class Whack_a_mole():
                 sys.exit()
 
     def _update_screen(self):
-        self.screen.blit(self.background, (0, 0))
-        self.hammer.blitme()
-        pygame.display.flip()
+        self.screen.blit(self.background, (0, 0))  # 绘制背景
+        self.hammer.blitme()  # 绘制锤子
+        pygame.display.flip()  # 更新屏幕
 
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
-    whack_a_mole = Whack_a_mole()
-    whack_a_mole.run_game()
+    whack_a_mole = Whack_a_mole()  # 创建游戏对象
+    whack_a_mole.run_game()  # 运行游戏
