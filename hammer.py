5db28e5c262f6fdd554df4b881c6e922f0202708
xiaobai-V <805892347@qq.com>
2023-04-23 15:48:23 +0800

    plot hammer in fixed position

--- hammer.py
+++ hammer.py
@@ -1,6 +1,32 @@
-class Hammer():
-    def __init__(self):
-        pass
+import pygame
+import random
 
-    def move(self):
-        pass
+
+class Hammer():
+    def __init__(self, game):
+        self.screen = game.screen
+        self.screen_rect = game.screen.get_rect()
+
+        self.image = pygame.image.load('hammer.png')
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        # self.rect = self.image.get_rect()
+
+        # self.rect.midbottom = self.screen_rect.midbottom
+
+        self.VRX = 200
+        self.VRY = 200
+        self.x = int(self.screen_rect.width * self.VRX / 255)
+        self.y = int(self.screen_rect.height * self.VRY / 255)
+
+        self.position = (self.x, self.y)
+
+    def blitme(self):
+        self.screen.blit(self.image, self.position)
+
+    def update(self):
+        # 树莓派上改用adc读取
+        self.VRX = 200
+        self.VRY = 200
+        self.x = int(self.screen_rect.width * self.VRX / 255)
+        self.y = int(self.screen_rect.height * self.VRY / 255)
+        self.position = (self.x, self.y)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
-import hammer
 import sys
+import time
+
+from hammer import Hammer
 
 
 class Whack_a_mole():
@@ -13,19 +15,30 @@ class Whack_a_mole():
 
         self.background = pygame.image.load("bg.png")
 
-        def run_game(self):
-            while True:
-                self._check_events()
-                self._update_screen()
+        self.hammer = Hammer(self)
 
-        def _check_events(self):
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+        self.VRX = 123
+        self.VRY = 123
 
-        def _update_screen(self):
-            self.screen.fill(self.background)
-            pygame.display.flip()
+    def run_game(self):
+        while True:
+            self._check_joystick()
+            self._check_events()
+            self._update_screen()
+            time.sleep(0.5)
+
+    def _check_joystick(self):
+        pass
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        self.screen.blit(self.background, (0, 0))
+        self.hammer.blitme()
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
